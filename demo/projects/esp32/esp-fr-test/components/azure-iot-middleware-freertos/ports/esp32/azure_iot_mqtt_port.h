fe99a2f73b81ed02f4bcfc730e81fe97782ac590
Tara W <tawalker@microsoft.com>
2021-06-07 13:41:46 -0400

    Adding my build testing project, Making sure ESP IDF is picking up azure freertos middleware w/ESP components for freertos, mqtt, lwip, etc. and not coreMQTT, freertos from other sources-TEW

---
+++ demo/projects/esp32/esp-fr-test/components/azure-iot-middleware-freertos/ports/esp32/azure_iot_mqtt_port.h
@@ -0,0 +1,11 @@
+#ifndef AZURE_IOT_MQTT_PORT_H
+#define AZURE_IOT_MQTT_PORT_H
+
+#include <stdio.h>
+
+#include "mqtt_client.h"
+
+/* Maps MQTTContext directly to AzureIoTMQTT */
+typedef struct esp_mqtt_client_handle_t  AzureIoTMQTT_t;
+
+#endif // AZURE_IOT_MQTT_PORT_H
