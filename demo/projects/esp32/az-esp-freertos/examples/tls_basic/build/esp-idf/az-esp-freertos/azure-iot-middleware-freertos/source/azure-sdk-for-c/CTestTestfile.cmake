f069f2ad18e3312e074626061c12f01cf9193521
Tara W <tawalker@microsoft.com>
2021-06-16 04:59:40 -0400

    Added cert folder/file for RootCA-TEW

--- demo/projects/esp32/az-esp-freertos/examples/tls_basic/build/esp-idf/az-esp-freertos/azure-iot-middleware-freertos/source/azure-sdk-for-c/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Tara/Code/az-esp-freertos/azure-iot-middleware-freertos/libraries/azure-sdk-for-c
-# Build directory: /mnt/c/Tara/Code/az-esp-freertos/examples/tls_basic/build/esp-idf/az-esp-freertos/azure-iot-middleware-freertos/source/azure-sdk-for-c
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("sdk/src/azure/core")
-subdirs("sdk/src/azure/iot")
-subdirs("sdk/src/azure/platform")
